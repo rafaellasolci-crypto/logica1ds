d5cac052cc8f6916a598ec02020d1ee59856b71c
rafaellasolci-crypto <rafaella.solci@escola.pr.gov.br>
2025-12-15 12:01:59 -0300

    Create testesCondicionais.js

---
+++ testesCondicionais.js
@@ -0,0 +1,56 @@
+dia = "Quarta"
+if dia == "Sábado" or dia == "Domingo":
+    print("Final de semana")
+else:
+    print("Dia útil")
+
+
+idade = 65
+
+if idade >= 18 and idade < 60:
+
+    print("Adulto")
+
+elif idade >= 60:
+
+    print("Idoso")
+
+else:
+
+    print("Menor de idade")
+
+
+saldo = 50
+
+compra = 80
+
+if saldo >= compra:
+
+    print("Compra aprovada")
+
+else:
+
+    print("Saldo insuficiente")
+
+
+dia = "Segunda"
+if dia == "Segunda":
+    print("Início da semana")
+elif dia == "Sexta":
+    print("Fim da semana")
+else:
+    print("Meio da semana")
+
+senha_correta = "admin"
+senha_digitada = "user"
+if senha_digitada == senha_correta:
+    print("Acesso liberado")
+else:
+    print("Acesso negado")
+
+numero = 10
+if numero % 2 == 0:
+    print("Par")
+else:
+    print("Ímpar")
+
